3b7745632498cbe4be69073b23684711e04e5992
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2024-01-29 14:37:30 +0100

    enemy can now attack

--- Enemy.cs
+++ Enemy.cs
@@ -3,16 +3,92 @@ using System;
 
 public partial class Enemy : CharacterBody2D
 {
+    [Export]
+	public PackedScene projectileScene {get; set;}
+    private Timer attackTimer;
     private int speed = 50;
+    private State currentState;
+    private RayCast2D ray;
     public Player target;
+    
+
+    enum State{
+        PLAYER_SEEN = 0,
+        PLAYER_HIDDEN = 1
+    }
 
     public override void _Ready(){
-
+        currentState = State.PLAYER_HIDDEN;
+        ray = GetNode<RayCast2D>("RayCast2D");
+        attackTimer = GetNode<Timer>("AttackCD");
     }
 
     public override void _Process(double delta){
-        GD.Print(target.Position);
-        Velocity = (target.Position - Position).Normalized()*speed;
+        //GD.Print(target.Position);
+        //GetNode<Timer>("AttackCD").Start(0.5);
+        ray.TargetPosition = target.Position - Position;
+        if(ray.IsColliding() && ray.GetCollider() == target){
+            setState(State.PLAYER_SEEN);
+        } else {
+            setState(State.PLAYER_HIDDEN);
+        }
+
+        if(currentState == State.PLAYER_SEEN){
+            Velocity = (target.Position - Position).Normalized()*speed;
+        } else {
+            Velocity = Vector2.Zero;
+        }
+
         MoveAndSlide();
     }
+
+    private void setState(State newState){
+        if(currentState != newState){
+            currentState = newState;
+            if(newState == State.PLAYER_SEEN){
+                attackTimer.Start(1);
+            } else {
+                attackTimer.Stop();
+            }
+        }
+    }
+
+    public void OnPlayerSeen(Node2D body){
+        if(body == target){
+            GD.Print("The player has been discoverd");
+            setState(State.PLAYER_SEEN);
+            ray.Enabled = true;
+        }
+    }
+
+    public void OnPlayerHide(Node2D body){
+        if(body == target){
+            GD.Print("The player has hidden");
+            setState(State.PLAYER_HIDDEN);
+            ray.Enabled = false;
+        }
+    }
+
+    public void OnAttackCDTimeout(){
+        GD.Print("Attack");
+        attackTimer.Start(1);
+        var direction = (target.Position - Position).Normalized();
+        ShootProjectile(direction);
+
+    }
+
+    public void ShootProjectile(Vector2 direction){
+        
+		var projectile = projectileScene.Instantiate<RigidBody2D>();
+		Vector2 projectielPosition = direction * 100;
+        projectile.Position = projectielPosition;
+		Vector2 projectileVelocity = direction * 400;
+        projectile.LinearVelocity = projectileVelocity;
+        AddChild(projectile);
+        /*
+		Vector2 projectileVelocity = projectile.LinearVelocity*playerDirection;
+		projectile.LinearVelocity = projectileVelocity;
+		AddChild(projectile);
+        */
+	}
 }
